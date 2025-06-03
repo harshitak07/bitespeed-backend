09b78a21ca3b9053715e7d9806962efe4cb02e58
harshitak07 <harshitakancharana07@gmail.com>
2025-06-03 15:00:24 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -5,13 +5,16 @@ const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  port:process.env.MYSQLPORT,
+  port: process.env.MYSQLPORT,
   database: process.env.MYSQL_DATABASE,
 });
 
 db.connect((err) => {
-  if (err) throw err;
-  console.log('✅ Connected to MySQL Database');
+  if (err) {
+    console.error('Failed to connect to MySQL Database:', err);
+    process.exit(1);
+  }
+  console.log('Connected to MySQL Database');
 });
 
 module.exports = db;
